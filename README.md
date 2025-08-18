9a13ef8ea9f81aee708f14f30e3fa55df708781d
Khiem Doan <doankhiem.crazy@gmail.com>
2025-08-18 15:24:30 +0700

    Init project
    Signed-off-by: Khiem Doan <doankhiem.crazy@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# starrocks-minio-cluter-docker-compose
+# StarRocks Minio Cluter inside Docker Compose
+
 Simple StarRocks and MinIO cluster inside Docker Compose.
+
+```sh
+docker exec -it starrocks-leader bash
+mysql -P9030 -h 127.0.0.1 -u root --prompt="StarRocks > " -p
+# without password
+```
+
+```sql
+SET PASSWORD = PASSWORD('$w0rdf1sh');
+```
+
+```sql
+CREATE DATABASE IF NOT EXISTS warehouse;
+```
---
+++ mkdirs.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+set -e
+
+# Data directory
+mkdir -p .data
+sudo chown 1000:1000 -R .data
+
+# StarRocks
+mkdir -p .data/starrocks-leader
+sudo chown 1000:1000 -R .data/starrocks-leader
+
+mkdir -p .data/starrocks-follower-1
+sudo chown 1000:1000 -R .data/starrocks-follower-1
+
+mkdir -p .data/starrocks-follower-2
+sudo chown 1000:1000 -R .data/starrocks-follower-2
+
+mkdir -p .data/starrocks-follower-3
+sudo chown 1000:1000 -R .data/starrocks-follower-3
+
+mkdir -p .data/starrocks-be-1
+sudo chown 1000:1000 -R .data/starrocks-be-1
+
+mkdir -p .data/starrocks-be-2
+sudo chown 1000:1000 -R .data/starrocks-be-2
+
+mkdir -p .data/starrocks-be-3
+sudo chown 1000:1000 -R .data/starrocks-be-3
+
+mkdir -p .data/starrocks-be-4
+sudo chown 1000:1000 -R .data/starrocks-be-4
+
+mkdir -p .data/starrocks-be-5
+sudo chown 1000:1000 -R .data/starrocks-be-5
+
+mkdir -p .data/starrocks-be-6
+sudo chown 1000:1000 -R .data/starrocks-be-6
+
+mkdir -p .data/minio1-1
+sudo chown 0:0 -R .data/minio1-1
+
+mkdir -p .data/minio1-2
+sudo chown 0:0 -R .data/minio1-2
+
+mkdir -p .data/minio1-3
+sudo chown 0:0 -R .data/minio1-3
+
+mkdir -p .data/minio1-4
+sudo chown 0:0 -R .data/minio1-4
+
+mkdir -p .data/minio2-1
+sudo chown 0:0 -R .data/minio2-1
+
+mkdir -p .data/minio2-2
+sudo chown 0:0 -R .data/minio2-2
+
+mkdir -p .data/minio2-3
+sudo chown 0:0 -R .data/minio2-3
+
+mkdir -p .data/minio2-4
+sudo chown 0:0 -R .data/minio2-4
+
+mkdir -p .data/minio3-1
+sudo chown 0:0 -R .data/minio3-1
+
+mkdir -p .data/minio3-2
+sudo chown 0:0 -R .data/minio3-2
+
+mkdir -p .data/minio3-3
+sudo chown 0:0 -R .data/minio3-3
+
+mkdir -p .data/minio3-4
+sudo chown 0:0 -R .data/minio3-4
+
+mkdir -p .data/minio4-1
+sudo chown 0:0 -R .data/minio4-1
+
+mkdir -p .data/minio4-2
+sudo chown 0:0 -R .data/minio4-2
+
+mkdir -p .data/minio4-3
+sudo chown 0:0 -R .data/minio4-3
+
+mkdir -p .data/minio4-4
+sudo chown 0:0 -R .data/minio4-4
+
+mkdir -p .data/minio5-1
+sudo chown 0:0 -R .data/minio5-1
+
+mkdir -p .data/minio5-2
+sudo chown 0:0 -R .data/minio5-2
+
+mkdir -p .data/minio5-3
+sudo chown 0:0 -R .data/minio5-3
+
+mkdir -p .data/minio5-4
+sudo chown 0:0 -R .data/minio5-4
