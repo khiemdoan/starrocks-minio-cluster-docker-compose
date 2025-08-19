5327ed5300d14c36a97c8c833341f6cdb62aeff4
Khiem Doan <doankhiem.crazy@gmail.com>
2025-08-19 11:30:14 +0700

    Use non-root user to run services
    Signed-off-by: Khiem Doan <doankhiem.crazy@gmail.com>

--- mkdirs.sh
+++ mkdirs.sh
@@ -19,79 +19,79 @@ mkdir -p .data/starrocks-follower-3
 sudo chown 1000:1000 -R .data/starrocks-follower-3
 
 mkdir -p .data/starrocks-cn-1
-sudo chown 0:0 -R .data/starrocks-cn-1
+sudo chown 1000:1000 -R .data/starrocks-cn-1
 
 mkdir -p .data/starrocks-cn-2
-sudo chown 0:0 -R .data/starrocks-cn-2
+sudo chown 1000:1000 -R .data/starrocks-cn-2
 
 mkdir -p .data/starrocks-cn-3
-sudo chown 0:0 -R .data/starrocks-cn-3
+sudo chown 1000:1000 -R .data/starrocks-cn-3
 
 mkdir -p .data/starrocks-cn-4
-sudo chown 0:0 -R .data/starrocks-cn-4
+sudo chown 1000:1000 -R .data/starrocks-cn-4
 
 mkdir -p .data/starrocks-cn-5
-sudo chown 0:0 -R .data/starrocks-cn-5
+sudo chown 1000:1000 -R .data/starrocks-cn-5
 
 mkdir -p .data/starrocks-cn-6
-sudo chown 0:0 -R .data/starrocks-cn-6
+sudo chown 1000:1000 -R .data/starrocks-cn-6
 
 mkdir -p .data/minio1-1
-sudo chown 0:0 -R .data/minio1-1
+sudo chown 1000:1000 -R .data/minio1-1
 
 mkdir -p .data/minio1-2
-sudo chown 0:0 -R .data/minio1-2
+sudo chown 1000:1000 -R .data/minio1-2
 
 mkdir -p .data/minio1-3
-sudo chown 0:0 -R .data/minio1-3
+sudo chown 1000:1000 -R .data/minio1-3
 
 mkdir -p .data/minio1-4
-sudo chown 0:0 -R .data/minio1-4
+sudo chown 1000:1000 -R .data/minio1-4
 
 mkdir -p .data/minio2-1
-sudo chown 0:0 -R .data/minio2-1
+sudo chown 1000:1000 -R .data/minio2-1
 
 mkdir -p .data/minio2-2
-sudo chown 0:0 -R .data/minio2-2
+sudo chown 1000:1000 -R .data/minio2-2
 
 mkdir -p .data/minio2-3
-sudo chown 0:0 -R .data/minio2-3
+sudo chown 1000:1000 -R .data/minio2-3
 
 mkdir -p .data/minio2-4
-sudo chown 0:0 -R .data/minio2-4
+sudo chown 1000:1000 -R .data/minio2-4
 
 mkdir -p .data/minio3-1
-sudo chown 0:0 -R .data/minio3-1
+sudo chown 1000:1000 -R .data/minio3-1
 
 mkdir -p .data/minio3-2
-sudo chown 0:0 -R .data/minio3-2
+sudo chown 1000:1000 -R .data/minio3-2
 
 mkdir -p .data/minio3-3
-sudo chown 0:0 -R .data/minio3-3
+sudo chown 1000:1000 -R .data/minio3-3
 
 mkdir -p .data/minio3-4
-sudo chown 0:0 -R .data/minio3-4
+sudo chown 1000:1000 -R .data/minio3-4
 
 mkdir -p .data/minio4-1
-sudo chown 0:0 -R .data/minio4-1
+sudo chown 1000:1000 -R .data/minio4-1
 
 mkdir -p .data/minio4-2
-sudo chown 0:0 -R .data/minio4-2
+sudo chown 1000:1000 -R .data/minio4-2
 
 mkdir -p .data/minio4-3
-sudo chown 0:0 -R .data/minio4-3
+sudo chown 1000:1000 -R .data/minio4-3
 
 mkdir -p .data/minio4-4
-sudo chown 0:0 -R .data/minio4-4
+sudo chown 1000:1000 -R .data/minio4-4
 
 mkdir -p .data/minio5-1
-sudo chown 0:0 -R .data/minio5-1
+sudo chown 1000:1000 -R .data/minio5-1
 
 mkdir -p .data/minio5-2
-sudo chown 0:0 -R .data/minio5-2
+sudo chown 1000:1000 -R .data/minio5-2
 
 mkdir -p .data/minio5-3
-sudo chown 0:0 -R .data/minio5-3
+sudo chown 1000:1000 -R .data/minio5-3
 
 mkdir -p .data/minio5-4
-sudo chown 0:0 -R .data/minio5-4
+sudo chown 1000:1000 -R .data/minio5-4
